b9a0dcd074fe0a958032d40b598b9e0bf5a1f0db
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-06 16:02:52 +0900

    _

--- pages/me.tsx
+++ pages/me.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { ComponentProps } from "react";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { create } from "~/useCase/useMe";
+import { create } from "~/controller/me";
 
 type Props = ComponentProps<typeof import("~/ui/components/Me")["Me"]>;
 const Me = dynamic<Props>(
