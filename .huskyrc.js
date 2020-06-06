b29ed2ee8e95c5e76a32a0a36e451b7ab0a39ed4
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-06 22:27:41 +0900

    _

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,13 +2,15 @@ module.exports = {
     hooks: {
       "pre-commit": `
         "yarn madge:src && \"
-        "yarn madge:src/controller && \"
-        "yarn madge:src/domain && \"
-        "yarn madge:src/infra && \"
-        "yarn madge:src/ui && \"
-        "yarn madge:src/useCase && \"
+        
         git add ./doc && \
         yarn lint-staged
         `,
     },
 }
+
+// "yarn madge:src/controller && \"
+//         "yarn madge:src/domain && \"
+//         "yarn madge:src/infra && \"
+//         "yarn madge:src/ui && \"
+//         "yarn madge:src/useCase && \"
