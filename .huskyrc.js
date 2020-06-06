298340268a3d8cea6ff103179d2dd368fc8e5c72
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-06 22:15:21 +0900

    _

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     hooks: {
       // "pre-commit": "yarn lint-staged",
-      "pre-commit": "yarn madge:src && git add ./doc/src/graph.svg && git commit -m '_' --no-verify && yarn lint-staged",
+      "pre-commit": "yarn madge:src && git add ./doc/src/graph.svg && yarn lint-staged",
     },
 }
