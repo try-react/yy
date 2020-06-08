c799f4295f4d176bb68416a48cf384506057e60e
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-08 12:15:22 +0900

    _

--- src/useCase/useMe/useMe.ts
+++ src/useCase/useMe/useMe.ts
@@ -1,25 +1,30 @@
-import { useEffect } from "react";
 import { UseMe } from "./type";
 import { DomainData } from "~/shared/typeGuard/read/Data";
+import { useAsync } from "react-use";
 
 export const useMe: UseMe = ({ service, initData }) => {
-  useEvent(service);
+  const { eventState } = useEvent(service);
+
+  if (eventState.value instanceof DomainData) {
+    console.log(eventState.value); // any
+  }
+
   return { ...initData };
 };
 
 const useEvent = (service: Parameters<UseMe>[0]["service"]) => {
-  useEffect(() => {
-    service
-      .fetch()
-      .then((r) => {
-        if (r instanceof DomainData) {
-          console.log(r.value);
-          return;
-        }
-        Promise.reject(r);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, [service]);
+  const state = useAsync(
+    () =>
+      service
+        .fetch()
+        .then((r) => {
+          if (r instanceof DomainData) {
+            return r;
+          }
+          return Promise.reject(r);
+        })
+        .catch((e) => e),
+    [service]
+  );
+  return { eventState: state };
 };
