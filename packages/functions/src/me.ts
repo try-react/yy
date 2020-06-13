0bf60fb4d16f40467c8ede8bd2df45559b7121b9
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-13 23:15:04 +0900

    :wrench: 設定変更

--- packages/functions/src/me.ts
+++ packages/functions/src/me.ts
@@ -1,17 +1,16 @@
 import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda";
 
-const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
+// const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
 
 type HelloResponse = {
   statusCode: number;
   body: string;
 };
-export const handler: Handler = async (
+export const handler: Handler = (
   event: APIGatewayEvent,
   context: Context,
   callback: Callback
 ) => {
-  await sleep();
   const response: HelloResponse = {
     statusCode: 200,
     body: JSON.stringify({
