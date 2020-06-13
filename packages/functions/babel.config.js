fad87b44833ced6dea6d85588ede365da7872451
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-14 00:11:27 +0900

    :wrench: (functions) 設定をごにょる

--- packages/functions/babel.config.js
+++ packages/functions/babel.config.js
@@ -1,4 +1,9 @@
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = {
   presets: ["@babel/preset-typescript", "@babel/preset-env"],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-object-assign",
+    "@babel/plugin-proposal-object-rest-spread",
+  ],
 };
