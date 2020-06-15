40cc6b25466105469938c937963f3ea2618206a2
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-15 14:55:05 +0900

    :fire: (functions) 不要なコードの削除

--- packages/functions/src/me.ts
+++ packages/functions/src/me.ts
@@ -1,26 +1,22 @@
-import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda";
+import type { Handler } from "aws-lambda";
 
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
 
-type HelloResponse = {
-  statusCode: number;
-  body: string;
-};
-export const handler: Handler = async (
-  event: APIGatewayEvent,
-  context: Context,
-  callback: Callback
-) => {
-  await sleep();
+const addresses = [
+  "ABC県 XYZ市",
+  "Github県 TypeScript市",
+  "React県 JavaScript市",
+  "FizzBuzz県 Fizz市",
+];
 
-  const response: HelloResponse = {
+export const handler: Handler = async () => {
+  await sleep();
+  return {
     statusCode: 200,
     body: JSON.stringify({
-      id_x_: 123,
-      name_x_: "tommy",
-      address_x_: `Github星 CleanArchitecture島 DDD丁目 ${new Date().getMilliseconds()}`,
+      id_x_: 100,
+      name_x_: "Tommy",
+      address_x_: addresses[Math.floor(Math.random() * addresses.length)],
     }),
   };
-
-  callback(undefined, response);
 };
