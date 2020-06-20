bdd256b2073aa6fb063d4b97014f0a8896ff456c
baronTommy <16768208+baronTommy@users.noreply.github.com>
2020-06-21 01:19:39 +0900

    :poop: (front) 検証コード

--- packages/front/pages/me.tsx
+++ packages/front/pages/me.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { ComponentProps } from "react";
 import type { NextPage } from "next";
-import type { UseCase } from "~/presenter/components/ecosystem/Me/Container/selector";
 import type { Container as ContainerType } from "~/presenter/components/ecosystem/Me/Container";
 import dynamic from "next/dynamic";
 import { selector } from "~/presenter/components/ecosystem/Me/Container";
@@ -9,17 +8,29 @@ import { useReRender } from "~/presenter/hooks/useReRender";
 import { interactor } from "~/useCase/me/interactor";
 import { repository } from "~/gateway/repository/me";
 
+// import { Container } from "~/presenter/components/ecosystem/Me";
+
 const Container = dynamic<ComponentProps<typeof ContainerType>>(
   () => import("~/presenter/components/ecosystem/Me").then((_) => _.Container),
   { ssr: false }
 );
 
-const Page: NextPage = () => {
-  const useCase: UseCase = {
-    ...interactor({ repository }),
-    ...useReRender(),
-  };
-  return <Container Content={selector({ useCase })} />;
+const Page: NextPage = (x) => {
+  console.log(x);
+  return <Me />;
 };
 
 export default Page;
+
+const Me = () => (
+  <Container
+    Content={selector({
+      useCase: {
+        ...interactor({ repository }),
+        ...useReRender(),
+      },
+    })}
+  />
+);
+
+Page.getInitialProps = () => Promise.resolve({ props: { x: "abc" } });
